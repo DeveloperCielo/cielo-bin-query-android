1df25f615bf1983c106d1efa147d773c3c8d4c69
Maurici Ferreira Junior <m.f.j@hotmail.com>
2020-05-15 20:23:47 -0300

    Added logging interceptor

--- cielobinquery/src/main/java/br/com/cielo/cielobinquery/internal/network/WebClient.kt
+++ cielobinquery/src/main/java/br/com/cielo/cielobinquery/internal/network/WebClient.kt
@@ -1,6 +1,8 @@
 package br.com.cielo.cielobinquery.internal.network
 
+import br.com.cielo.cielobinquery.BuildConfig
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit.Builder
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -14,8 +16,15 @@ internal class WebClient(apiUrl: String) {
     private val httpClient = OkHttpClient().newBuilder()
 
     fun <T> createService(service: Class<T>): T {
+        val logging = HttpLoggingInterceptor()
+        logging.level =
+            if (BuildConfig.DEBUG)
+                HttpLoggingInterceptor.Level.BODY
+            else
+                HttpLoggingInterceptor.Level.NONE
 
         val client = httpClient
+            .addInterceptor(logging)
             .connectTimeout(45, TimeUnit.SECONDS)
             .readTimeout(45, TimeUnit.SECONDS)
             .writeTimeout(45, TimeUnit.SECONDS)
